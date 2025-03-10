65afaf79d93e693283cf178cdf3efa4fc910ba9e
ferriprasetya <feriadiprasetya12@gmail.com>
2025-03-10 22:53:44 +0700

    feat: feat/image-to-cdn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dfhxl1ruc/image/upload/v1741621368/**',
+        search: '',
+      },
+    ],
+  },
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
